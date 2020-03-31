360ebb19f16109a11f94b5bb0f5ae916ad54f944
Rohit Gujarathi <gujju.rohit@gmail.com>
2020-03-31 22:48:14 +0530

    Changed to FreeRTOS based blinky

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
 #include "stm32f4xx_hal.h"
 
 void SystemClock_Config(void);
@@ -8,12 +12,19 @@ static GPIO_InitTypeDef  GPIO_InitStruct;
 #define LED1_PIN GPIO_PIN_13
 #define LED1_PORT GPIOG
 
+static void LedBlinky_Task(void *pvParameters){
+	while (1)
+	{
+		HAL_GPIO_TogglePin(LED1_PORT, LED1_PIN);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+
 int main ( void )
 {
 	HAL_Init();
 	SystemClock_Config();
 
-	
 	__HAL_RCC_GPIOG_CLK_ENABLE();
 	
 	GPIO_InitStruct.Pin   = LED1_PIN;
@@ -22,22 +33,29 @@ int main ( void )
 	GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
 
 	HAL_GPIO_Init(LED1_PORT, &GPIO_InitStruct);
-	
+
+	xTaskCreate( LedBlinky_Task,						/* The function that implements the task. */
+				"LedBlinky", 							/* The text name assigned to the task - for debug only as it is not used by the kernel. */
+				configMINIMAL_STACK_SIZE, 				/* The size of the stack to allocate to the task. */
+				NULL, 									/* The parameter passed to the task - just to check the functionality. */
+				3, 										/* The priority assigned to the task. */
+				NULL );									/* The task handle is not required, so NULL is passed. */
+
+	vTaskStartScheduler();
+
 	while (1)
 	{
-		HAL_GPIO_TogglePin(LED1_PORT, LED1_PIN);
-		/* Insert delay 100 ms */
-		HAL_Delay(1000);
+		
 	}
 	
 	return 0;
 }
 
-void SysTick_Handler(void){
+//~ void SysTick_Handler(void){
 
-	HAL_IncTick();
+	//~ HAL_IncTick();
 
-}
+//~ }
 
 void SystemClock_Config(void)
 {
@@ -50,29 +68,42 @@ void SystemClock_Config(void)
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
   /** Initializes the CPU, AHB and APB busses clocks 
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 128;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
-    while(1){
-		
-	}
+    Error_Handler();
   }
   /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
-    while(1){
+    Error_Handler();
+  }
+}
+
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+	while(1){
 		
 	}
-  }
+
+  /* USER CODE END Error_Handler_Debug */
 }
